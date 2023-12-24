3188dfbe43b19116ba6cbcbbf2a47f02048fc661
bendibdounia <bendib.dounia@gmail.com>
2023-12-24 07:53:44 -0800

    hello hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-#script for two route flask application
+"""
+script for two route flask application
+"""
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """return hello hbnb"""
     return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """return hbnb"""
     return "HBNB"
 
 if __name__ == '__main__':
