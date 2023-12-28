0e5cda1a2b0b99be8c7c8ac35789d8ab0752bad3
bendibdounia <bendib.dounia@gmail.com>
2023-12-28 10:07:23 -0800

    templates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -24,6 +24,7 @@ def c_index(text):
     """return C followed by the value of text"""
     return f"C {text.replace('_', ' ')}"
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/(<text>)', strict_slashes=False)
 def python_index(text='is cool'):
